19336264e5c8578650bcf8dacf904267da4d91d8
Carolina Munoz <carito.camo@gmail.com>
2021-11-23 01:57:38 -0300

    Se agrega useEffect con la conexion a firebase

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import React, {useEffect} from "react";
 import {firestore} from "./firebase"
 
 function App() {
+
+  useEffect(()=>{
+    firestore
+    .collection("tweets")
+    .get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        console.log(doc.data());
+      })
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
