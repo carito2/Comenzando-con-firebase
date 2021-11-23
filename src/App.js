3f20d32e32e69c54827244f4f8b05e9560e2a14d
Carolina Munoz <carito.camo@gmail.com>
2021-11-23 16:51:45 -0300

    Creacion de formulario

--- src/App.js
+++ src/App.js
@@ -1,37 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useEffect} from "react";
-import {firestore} from "./firebase"
+import React, {useEffect, useState} from "react";
+import { firestore } from "./firebase.js"
+import Tweets from "./components/Tweets"
 
 function App() {
+  const [tweets, setTweets] = useState([]);
+  const [tweet, setTweet] = useState({
+    tweet: "",
+    autor: ""
+  });
 
   useEffect(()=>{
     firestore
     .collection("tweets")
     .get()
     .then((snapshot) => {
-      snapshot.forEach((doc) => {
-        console.log(doc.data());
+      const tweets = snapshot.docs.map((doc) => {
+        console.log(doc.data())
+        return {
+          autor: doc.data().autor,
+          tweet: doc.data().tweet,
+          id: doc.data().id
+        }
       })
-    })
+      setTweets(tweets);
+    });
   }, [])
+
+  const handleChange = (e) => {
+    let newTweet = {
+      ...tweet,
+      [e.target.name]: e.target.value
+    }
+    setTweet(newTweet);
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="appForm">
+        <textarea cols="30" rows="5" placeholder="Escribe un tweet..." value={tweet.tweet} onChange={handleChange} name="tweet"></textarea>
+        <div className="inputAndButton">
+          <input type="text" placeholder="Persona autora" value={tweet.autor} onChange={handleChange} name="autor"></input>
+          <button>Enviar Tweet</button>
+        </div>
+      </form>
+      <Tweets tweets={tweets}/>
     </div>
   );
 }
